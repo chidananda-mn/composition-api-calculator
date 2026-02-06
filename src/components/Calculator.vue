944ee414984baf3fd87c3b69b87750a2968a5047
Chidananda <39602044+chidananda-mn@users.noreply.github.com>
2026-02-06 17:28:40 +0530

    Add Calculator.vue component with logic using Vue 3 Composition API

---
+++ src/components/Calculator.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Calculator</h1>
+    <input type="number" v-model="num1" placeholder="Number 1">
+    <input type="number" v-model="num2" placeholder="Number 2">
+    <button @click="add">+</button>
+    <button @click="subtract">-</button>
+    <button @click="multiply">*</button>
+    <button @click="divide">/</button>
+    <h2>Result: {{ result }}</h2>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const num1 = ref(0);
+    const num2 = ref(0);
+    const result = ref(0);
+
+    const add = () => {
+      result.value = num1.value + num2.value;
+    };
+
+    const subtract = () => {
+      result.value = num1.value - num2.value;
+    };
+
+    const multiply = () => {
+      result.value = num1.value * num2.value;
+    };
+
+    const divide = () => {
+      result.value = num1.value / num2.value;
+    };
+
+    return { num1, num2, result, add, subtract, multiply, divide };
+  }
+};
+</script>
+
+<style>
+/* Add any styles for the calculator here */
+</style>
